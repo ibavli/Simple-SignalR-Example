fe5108a2591a7c2b99324224cb22e6ec93d9bfe6
ibavli <ibrahim.bavli35@gmail.com>
2018-08-23 01:03:55 +0300

    Created ChatHub.cs (SignalR Hub Class(v2))

--- SimpleSignalRExample/App_Start/Startup.cs
+++ SimpleSignalRExample/App_Start/Startup.cs
@@ -11,7 +11,7 @@ namespace SimpleSignalRExample.App_Start
     {
         public void Configuration(IAppBuilder app)
         {
-            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR();
         }
     }
 }
---
+++ SimpleSignalRExample/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SimpleSignalRExample.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string name, string message)
+        {
+            Clients.Others.GetMessageOther(name, message);
+
+            Clients.Caller.GetMessageCaller(message);
+        }
+    }
+}
